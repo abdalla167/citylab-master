2d14ba0c753219736d217d2788356f2531105101
abdalla167 <44808889+abdalla167@users.noreply.github.com>
2021-11-08 19:10:46 +0200

    abdallnew

--- app/src/main/java/com/medical/citylap/fragemnt/Profilefragment.java
+++ app/src/main/java/com/medical/citylap/fragemnt/Profilefragment.java
@@ -42,6 +42,8 @@ public String token;
 
     }
 
+    ////////////
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
